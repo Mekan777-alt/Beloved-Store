a3ee61515b81d4d971423f09ffd6948c652bb11b
Mekan777-alt <mekan2678@gmail.com>
2022-11-15 07:05:50 +0300

    add DEBUG and MEDIA_ROOT

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from products.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('basket/', include('baskets.urls', namespace='basket')),
     path('orders/', include('baskets.urls', namespace='orders')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
