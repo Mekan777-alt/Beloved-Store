8146cad3b78d8d7603ea1f7f93295c60d18fa01a
Mekan777-alt <mekan2678@gmail.com>
2022-12-09 01:17:19 +0300

    product.name and price remove

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Orders(models.Model):
+    pass
+    # name = models.Charfield()
+    # lastname =
+    # email =
+    # address =
+    # index =
+    # phone_number =
+    # comment =
